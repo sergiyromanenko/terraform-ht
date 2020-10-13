fa267c8a9ac6d982b05aaf194c213e64a5e5d0e3
sergiyromanenko <sergiy.romanenko@gmail.com>
2020-10-13 17:24:37 +0300

    Update main.tf

--- modules/mymodules/aws_network/main.tf
+++ modules/mymodules/aws_network/main.tf
@@ -51,7 +51,8 @@ resource "aws_route_table" "public_subnets" {
 }
 
 resource "aws_route_table_association" "public_routes" {
-  count          = length(aws_subnet.public_subnets[*].id)
+  count                   = length(var.public_subnet_cidrs)
+  #count          = length(aws_subnet.public_subnets[*].id)
   route_table_id = aws_route_table.public_subnets.id
   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
 }
